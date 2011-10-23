13dacb102d675e58065bf15c95a63e60981cd4b2
Ryan Schmukler <rschmukler@gmail.com>
2011-10-23 15:44:16 -0500

    Updated (barely) mem.c

--- linux/mem.c
+++ linux/mem.c
@@ -1,6 +1,21 @@
 #include "mem.h"
+#include <stdlib>
 
 int Mem_Init(int sizeOfRegion, int debug)
 {
+	mmap(NULL, )
+}
+
+void *Mem_Alloc(int size)
+{
+	
+}
+
+int Mem_Free(void *ptr)
+{
+	
+}
+
+void Mem_Dump(){
 	
 }
